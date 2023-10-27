2d8b5635bb9b3ad5d424b7c8f0c8c2d19dff98e7
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-10-27 07:17:18 +0530

    fix:🐍 env error, New:✨User level access on embeddings updated streamlit ui

--- chat_ui.py
+++ chat_ui.py
@@ -3,33 +3,58 @@ import os
 import openai
 import streamlit as st
 from dotenv import load_dotenv
-
-from chains_bot import get_answer, get_answer_from_agent, run_multiple_agents
+from key_store import (OPENAI_API_KEY,USER_ID)
+from chains_bot import get_answer
+from gen_embeddings import generateEmbeddings
 from supabase_db import getSimilarDocuments
+from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 
 load_dotenv()
 
+def getChatUi(user:str):
+            openai.api_key = OPENAI_API_KEY
+            if "openai_model" not in st.session_state:
+                st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
+            if prompt := st.chat_input("What is up?"):
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+
+                with st.chat_message("assistant"):
+                    response = get_answer(prompt,user)
+                    st.markdown(response["answer"])
+                    st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+                    
+
 st.title("GEN-TS HR Partner")
 
-openai.api_key = os.getenv("OPENAI_API_KEY", "default_key")
+user = st.text_input("Enter user id", value="dde6fc19-73fe-4ea6-a98b-a11485c710df",help="Pass the userId of which you are storing the document for. Other user's wont be able to access this doc then")
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+values = ['Yes','No']
+option = st.selectbox(
+    'Want to chat with previous docs',
+    (values ),index=0)
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+if option == 'No' and user !=None:
+    print(user)
+    pdf = st.file_uploader("Upload PDF Files", type=["pdf"], accept_multiple_files=False)
+    if pdf is not None:
+            generateEmbeddings(pdf,user)
+            getChatUi(user)
+else:
+    getChatUi(user)            
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        response = get_answer(prompt)
-        st.markdown(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        
+
--- gen_embeddings.py
+++ gen_embeddings.py
@@ -1,20 +1,38 @@
-
+from key_store import (OPENAI_API_KEY,USER_ID)
 import os
-
 from dotenv import load_dotenv
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders import PDFPlumberLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+from langchain.text_splitter import CharacterTextSplitter
 from supabase_db import saveDocVectorSupabase, saveToSupabase
+from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
+from langchain.schema.document import Document
 
-embeddingOpenAi= OpenAIEmbeddings()
+embeddingOpenAi= OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-# loader = PyPDFLoader('/Users/nomanmurtaza/Documents/Noman_Murtaza_CV.pdf')
-loader = CSVLoader(file_path="/Users/nomanmurtaza/Downloads/user details.csv")
+generated =False
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-pages = loader.load_and_split(text_splitter=text_splitter)
+def generateEmbeddings(pdf_bytes,user):
+  global generated
+  if generated == False:
+          # Initialize a list to store the pages
+    pages = []
+    
+    # Initialize PyPDF2 PdfFileReader
+    pdf_reader = PdfReader(pdf_bytes)
+    
+    # Iterate through the pages of the PDF
+    for index in range(0,len(pdf_reader.pages)):
+        page = pdf_reader.pages[index]
+        page_text = page.extract_text()
+        
+        # Create a metadata dictionary with "user_id"
+        metadata = {"user_id": user,"page":index}
+        
+        # Append the content and metadata to the pages list
+        pages.append(Document(page_content=page_text,metadata=metadata))
 
-#saveDocVectorSupabase(pages)
+    # Save the pages to Supabase using your saveDocVectorSupabase function
+    saveDocVectorSupabase(pages)
+    print("Added into supabase successfully")
+    generated = True
